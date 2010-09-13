8ae156b438484bc75440324cb1e40a4fc9d6c268
mirai <mirai@ccc6e944-403e-a54a-b5f3-c2babcd103ea>
2010-09-13 16:35:40 +0000

    MS: adding LICENSE; adding more demos
    git-svn-id: svn://ms72.weblink.ch/mirai/XLConnect-R@32 ccc6e944-403e-a54a-b5f3-c2babcd103ea

---
+++ demo/hideSheets.R
@@ -0,0 +1,41 @@
+# Hiding worksheets of an Excel file
+# 
+# Author: Martin Studer, Mirai Solutions GmbH
+###############################################################################
+
+require(XLConnect)
+
+# Excel workbook to write
+demoExcelFile <- "hide.xls"
+
+# Remove file if it already exists
+if(file.exists(demoExcelFile)) file.remove(demoExcelFile)
+
+# Load workbook (create if not existing)
+wb <- loadWorkbook(demoExcelFile, create = TRUE)
+
+# Write a couple of built-in data.frame's into sheets
+# with corresponding name
+for(obj in c("CO2", "airquality", "swiss")) {
+	createSheet(wb, name = obj)
+	writeWorksheet(wb, get(obj), sheet = obj)
+}
+
+# Hide sheet 'airquality';
+# the sheet may be unhidden by a user from within Excel
+hideSheet(wb, sheet = "airquality")
+# Alternatively: wb$hideSheet(sheet = "airquality")
+
+# Very-hide sheet 'swiss';
+# the sheet cannot be unhidden by a user from within Excel
+hideSheet(wb, sheet = "swiss", veryHidden = TRUE)
+# Alternatively: wb$hideSheet(sheet = "swiss", veryHidden = TRUE)
+
+# Save workbook (this actually writes the file to disk)
+saveWorkbook(wb)
+# Alternatively: wb$saveWorkbook()
+
+if(interactive()) {
+	answer <- readline("Open the created Excel file (y/n)? ")
+	if(answer == "y") shell.exec(file.path(getwd(), demoExcelFile))
+}
---
+++ demo/writeLargeData.R
@@ -0,0 +1,57 @@
+# Writing large datasets to Excel
+# 
+# Author: Martin Studer, Mirai Solutions GmbH
+###############################################################################
+
+# Increase Java's maximum heap space;
+# this option must be set before the underlying JVM is initialized
+# and therefore MUST happen before XLConnect is loaded
+options(java.parameters = "-Xmx1024m")
+
+if(any(is.element(c("package:XLConnect", "package:rJava"), search()))) {
+	msg <- paste(
+		"XLConnect and/or rJava are already attached.",
+		"You may reload these packages in order for the Java parameter setting",
+		"to take effect.",
+		sep = "\n"
+	)
+	warning(msg)
+}
+
+require(XLConnect)
+
+# Excel workbook to write
+demoExcelFile <- "large.xlsx"
+
+# Remove file if it already exists
+if(file.exists(demoExcelFile)) file.remove(demoExcelFile)
+
+# Create a large dummy data.frame
+set.seed(292)
+n <- 50000
+dfLarge <- data.frame(
+	A = rnorm(n),
+	B = sample(letters, size = n, replace = TRUE),
+	C = rnorm(n) > 0,
+	D = rep(as.Date("2010-09-17"), n),
+	E = rnorm(n),
+	F = sample(LETTERS, size = n, replace = TRUE),
+	G = 1:n
+)
+
+# Load workbook (create if not existing)
+wb <- loadWorkbook(demoExcelFile, create = TRUE)
+
+# Create a worksheet called 'Dummy'
+createSheet(wb, name = "Dummy")
+
+# Write large data.frame to worksheet 'Dummy' created above
+writeWorksheet(wb, dfLarge, sheet = "Dummy")
+
+# Save workbook (this actually writes the file to disk)
+saveWorkbook(wb)
+
+if(interactive()) {
+	answer <- readline("Open the created Excel file (y/n)? ")
+	if(answer == "y") shell.exec(file.path(getwd(), demoExcelFile))
+}
