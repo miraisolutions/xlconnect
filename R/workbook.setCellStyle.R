84b81993279ad500741acf7e07915db3469e4ce7
mirai <mirai@ccc6e944-403e-a54a-b5f3-c2babcd103ea>
2010-11-11 19:09:37 +0000

    MS: some package info updates; added setCellStyle
    git-svn-id: svn://ms72.weblink.ch/mirai/XLConnect-R@50 ccc6e944-403e-a54a-b5f3-c2babcd103ea

---
+++ R/workbook.setCellStyle.R
@@ -0,0 +1,48 @@
+#############################################################################
+#
+# XLConnect
+# Copyright (C) 2010 Mirai Solutions GmbH
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+#############################################################################
+#
+# TODO: Add Comment
+# 
+# Author: Martin Studer, Mirai Solutions GmbH
+#
+#############################################################################
+
+setGeneric("setCellStyle",
+		function(object, sheet, row, col, cellstyle) standardGeneric("setCellStyle"))
+
+setMethod("setCellStyle", 
+		signature(object = "workbook", sheet = "numeric", row = "numeric", col = "numeric", 
+			cellstyle = "cellstyle"), 
+		function(object, sheet, row, col, cellstyle) {
+			jTryCatch(object@jobj$setCellStyle(as.integer(sheet - 1), as.integer(row - 1),
+				as.integer(col - 1), cellstyle@jobj))
+		}
+)
+
+setMethod("setCellStyle", 
+		signature(object = "workbook", sheet = "character", row = "numeric", col = "numeric", 
+				cellstyle = "cellstyle"), 
+		function(object, sheet, row, col, cellstyle) {
+			jTryCatch(object@jobj$setCellStyle(sheet, as.integer(row - 1),
+							as.integer(col - 1), cellstyle@jobj))
+		}
+)
