c82152c92727bdaddad43a55ee26295a7d0d2d81
mirai <mirai@ccc6e944-403e-a54a-b5f3-c2babcd103ea>
2011-02-03 14:11:59 +0000

    TT: Add getCellStyle method to retrieve existing cell style objects
    git-svn-id: svn://ms72.weblink.ch/mirai/XLConnect-R@65 ccc6e944-403e-a54a-b5f3-c2babcd103ea

---
+++ R/workbook.getCellStyle.R
@@ -0,0 +1,38 @@
+#############################################################################
+#
+# XLConnect
+# Copyright (C) 2010 Mirai Solutions GmbH
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+#############################################################################
+#
+# Accessing named cell styles
+# 
+# Author: Martin Studer, Mirai Solutions GmbH
+#
+#############################################################################
+
+setGeneric("getCellStyle",
+		function(object, name) standardGeneric("getCellStyle"))
+
+setMethod("getCellStyle", 
+		signature(object = "workbook", name = "character"), 
+		function(object, name) {
+			jobj <- jTryCatch(object@jobj$getCellStyle(name))
+                        new("cellstyle", jobj = jobj)
+		}
+)
