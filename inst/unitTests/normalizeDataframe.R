15a65b61fcf4969efd1ba138555b733d8f222e1a
mirai <mirai@ccc6e944-403e-a54a-b5f3-c2babcd103ea>
2012-12-27 17:58:07 +0000

    MS: changing copyright year
    git-svn-id: svn://ms72.weblink.ch/mirai/XLConnect-R@300 ccc6e944-403e-a54a-b5f3-c2babcd103ea

--- inst/unitTests/normalizeDataframe.R
+++ inst/unitTests/normalizeDataframe.R
@@ -1,7 +1,7 @@
 #############################################################################
 #
 # XLConnect
-# Copyright (C) 2010-2012 Mirai Solutions GmbH
+# Copyright (C) 2010-2013 Mirai Solutions GmbH
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
