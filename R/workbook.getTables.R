fa904c72e8471eb3a4bc35fe25d3eba27278fcf3
Martin Studer <martin.remo.studer@gmail.com>
2013-12-31 13:27:32 +0100

    adding functionality to query tables

---
+++ R/workbook.getTables.R
@@ -0,0 +1,45 @@
+#############################################################################
+#
+# XLConnect
+# Copyright (C) 2010-2013 Mirai Solutions GmbH
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+#############################################################################
+#
+# Querying available Excel tables in a workbook
+# 
+# Author: Martin Studer, Mirai Solutions GmbH
+#
+#############################################################################
+
+setGeneric("getTables",
+	function(object, sheet) standardGeneric("getTables"))
+
+setMethod("getTables", 
+	signature(object = "workbook", sheet = "numeric"), 
+	function(object, sheet) {
+	  xlcCall(object, "getTables", as.integer(sheet - 1))
+	}
+)
+
+setMethod("getTables", 
+  signature(object = "workbook", sheet = "character"), 
+  function(object, sheet) {
+    xlcCall(object, "getTables", sheet)
+  }
+)
+
