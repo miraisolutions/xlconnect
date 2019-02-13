36e69a3fc84dd0cc95ec3cb4fe96b9264da6abdf
Roland Schmid <roland.schmid@mirai-solutions.com>
2019-02-13 12:02:06 +0100

    try javareconf for osx

--- travissetup.sh
+++ travissetup.sh
@@ -1,11 +1,16 @@
-# https://www.linuxuprising.com/2018/10/how-to-install-oracle-java-11-in-ubuntu.html
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+  R CMD javareconf
+  echo hello osx
+else
+  # https://www.linuxuprising.com/2018/10/how-to-install-oracle-java-11-in-ubuntu.html
 
-echo oracle-java11-installer shared/accepted-oracle-license-v1-2 select true | sudo /usr/bin/debconf-set-selections
+  echo oracle-java11-installer shared/accepted-oracle-license-v1-2 select true | sudo /usr/bin/debconf-set-selections
 
-# note that java10 is not available through this anymore
-sudo add-apt-repository ppa:linuxuprising/java -y
-sudo apt update -q
-# below triggers oracle-java11-set-default through 'Recommends'
-sudo apt --install-recommends install oracle-java11-installer
+  # note that java10 is not available through this anymore
+  sudo add-apt-repository ppa:linuxuprising/java -y
+  sudo apt update -q
+  # below triggers oracle-java11-set-default through 'Recommends'
+  sudo apt --install-recommends install oracle-java11-installer
+fi
 
 java -version
