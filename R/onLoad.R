ed4d53f70c313c0884f6ae02f6dba6c6af5e747b
spoltier <simon.poltier@mirai-solutions.com>
2020-01-29 18:38:35 +0100

    #97 parameterizable jar repo

--- R/onLoad.R
+++ R/onLoad.R
@@ -27,38 +27,43 @@
 #############################################################################
 
 .onLoad <- function(libname, pkgname) {
+  repo <- getOption("xlconnect.java.repo.url")
+  if (is.null(repo)) {
+    repo <- "https://repo1.maven.org/maven2"
+  }
+  apachePrefix <- paste0(repo, "/org/apache")
   poiPaths <- xlcEnsureDependenciesFor(cbind(
-      c("https://repo1.maven.org/maven2/org/apache/poi/poi-ooxml/4.1.1/poi-ooxml-4.1.1.jar", "poi-ooxml.jar"),
-      c("https://repo1.maven.org/maven2/org/apache/poi/poi/4.1.1/poi-4.1.1.jar", "poi.jar"),
-      c("https://repo1.maven.org/maven2/org/apache/poi/poi-ooxml-schemas/4.1.1/poi-ooxml-schemas-4.1.1.jar", "poi-ooxml-schemas.jar")
+      c(paste0(apachePrefix, "/poi/poi-ooxml/4.1.1/poi-ooxml-4.1.1.jar"), "poi-ooxml.jar"),
+      c(paste0(apachePrefix, "/poi/poi/4.1.1/poi-4.1.1.jar"), "poi.jar"),
+      c(paste0(apachePrefix, "/poi/poi-ooxml-schemas/4.1.1/poi-ooxml-schemas-4.1.1.jar"), "poi-ooxml-schemas.jar")
     ), "poi-4\\.[1-9].*",
     c("/usr/share/java/poi.jar", "/usr/share/java/poi-ooxml.jar", "/usr/share/java/poi-ooxml-schemas.jar"), libname, pkgname)
   compressPaths <- xlcEnsureDependenciesFor(cbind(
-      c("https://repo1.maven.org/maven2/org/apache/commons/commons-compress/1.19/commons-compress-1.19.jar", "commons-compress.jar")
+      c(paste0(apachePrefix, "/commons/commons-compress/1.19/commons-compress-1.19.jar"), "commons-compress.jar")
     ), "commons-compress-1\\.(1[8-9]|[2-9][0-9]).*",
     c("/usr/share/java/commons-compress.jar"), libname, pkgname)
   xmlPath <- xlcEnsureDependenciesFor(cbind(
-      c("https://repo1.maven.org/maven2/org/apache/xmlbeans/xmlbeans/3.1.0/xmlbeans-3.1.0.jar", "xmlbeans.jar")
+      c(paste0(apachePrefix, "/xmlbeans/xmlbeans/3.1.0/xmlbeans-3.1.0.jar"), "xmlbeans.jar")
   ), "xmlbeans-3\\..*",
     c("/usr/share/java/xmlbeans.jar"), libname, pkgname)
   collectionsPath <- xlcEnsureDependenciesFor(cbind(
-      c("https://repo1.maven.org/maven2/org/apache/commons/commons-collections4/4.4/commons-collections4-4.4.jar", "commons-collections4.jar")
+      c(paste0(apachePrefix, "/commons/commons-collections4/4.4/commons-collections4-4.4.jar"), "commons-collections4.jar")
     ), "commons-collections4-4\\.([2-9]|1[0-9]).*",
     c("/usr/share/java/commons-collections4.jar"), libname, pkgname)
   mathPath <- xlcEnsureDependenciesFor(cbind(
-      c("https://repo1.maven.org/maven2/org/apache/commons/commons-math3/3.6.1/commons-math3-3.6.1.jar", "commons-math3.jar")
+      c(paste0(apachePrefix, "/commons/commons-math3/3.6.1/commons-math3-3.6.1.jar"), "commons-math3.jar")
     ), "commons-math3-3\\.([6-9]|1[0-9]).*",
     c("/usr/share/java/commons-math3.jar"), libname, pkgname)
   codecPath <- xlcEnsureDependenciesFor(cbind(
-      c("https://repo1.maven.org/maven2/commons-codec/commons-codec/1.13/commons-codec-1.13.jar", "commons-math3.jar")
+      c(paste0(repo, "/commons-codec/commons-codec/1.13/commons-codec-1.13.jar"), "commons-math3.jar")
     ), "commons-codec-1\\.(1[1-9]|[2-9][0-9]).*",
     c("/usr/share/java/commons-codec.jar"), libname, pkgname)
   ooxmlSchemasPath <- xlcEnsureDependenciesFor(cbind(
-    c("https://repo1.maven.org/maven2/org/apache/poi/ooxml-schemas/1.4/ooxml-schemas-1.4.jar", "ooxml-schemas.jar")
+    c(paste0(apachePrefix, "/poi/ooxml-schemas/1.4/ooxml-schemas-1.4.jar"), "ooxml-schemas.jar")
   ), "ooxml-schemas-1\\.([4-9]|[1-9][0-9]).*",
   c("/usr/share/java/ooxml-schemas.jar"), libname, pkgname)
 
-  sharedPaths <- c(poiPaths, compressPaths, xmlPath, collectionsPath, mathPath, codecPath)
+  sharedPaths <- c(poiPaths, compressPaths, xmlPath, collectionsPath, mathPath, codecPath, ooxmlSchemasPath)
 	.jpackage(name = pkgname, jars = "*", morePaths = sharedPaths)
   
 	# Perform general XLConnect settings - pass package description
