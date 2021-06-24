8839eb61c7efac53052b83f47964b18edc7fb819
spoltier <simon.poltier@mirai-solutions.com>
2021-06-24 11:10:26 +0200

    gh #130 rename cloud build files

---
+++ cloudcheck.off.sh
@@ -0,0 +1,27 @@
+# apt-get update
+# apt install -y --no-install-recommends xzdec pandoc qpdf ghostscript
+# tlmgr init-usertree
+# tlmgr install datetime etoolbox fmtcount ulem pgf
+# install2.r rJava RUnit ggplot2 zoo
+
+# mkdir -p /tmp/results
+# cd /tmp/results
+# git clone --depth=1 git://github.com/miraisolutions/xlconnectjars.git
+# git clone --depth=1 git://github.com/miraisolutions/xlconnect.git
+
+# R CMD build --md5 xlconnectjars
+# R CMD check --as-cran XLConnectJars_*.tar.gz
+# R CMD INSTALL XLConnectJars_*.tar.gz
+
+# # Do not drop unit tests to run full test suite
+# cp xlconnect/.Rbuildignore .Rbuildignore.bak
+# sed -i "/unitTests/d" xlconnect/.Rbuildignore
+
+# R CMD build --compact-vignettes=both --md5 xlconnect
+# FULL_TEST_SUITE=1 R CMD check --as-cran XLConnect_*.tar.gz
+# R CMD INSTALL XLConnect_*.tar.gz
+
+# mv .Rbuildignore.bak xlconnect/.Rbuildignore
+# R CMD build --compact-vignettes --md5 xlconnect
+
+# tar -zcvf /exchange/results-$1.tar.gz XLConnectJars* XLConnect*
