48adf6796eed67f7f56d4f7e9300a4240b4dc7cd
Simon Poltier <simon.poltier@mirai-solutions.com>
2021-09-30 10:08:24 +0200

    Feature/#149 java17 (#150)
    * Patch dev version
    
    * test with java 17
    
    * Allow java 17
    
    * ubuntu 16.04 no longer supported

--- R/onLoad.R
+++ R/onLoad.R
@@ -46,7 +46,7 @@
         jvn <- as.numeric(twoFirst)
       }
     }
-    if (jvn<8 || jvn>16) stop(paste0("XLConnect is compatible with Java versions 8 to 16. Detected java version: ",jv))
+    if (jvn<8 || jvn>17) stop(paste0("XLConnect is compatible with Java versions 8 to 16. Detected java version: ",jv))
   }
   javaCheck()
   repo <- Sys.getenv("XLCONNECT_JAVA_REPO_URL")
