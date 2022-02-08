997a10d13aa8b95284ffdc990cab197df6b52d5f
Simon Poltier <simon.poltier@mirai-solutions.com>
2022-02-08 14:12:58 +0100

    #156 java version parsing with env vars (#157)
    * #156 test for java v. parsing with env vars
    
    * #156 use var everywhere - wip
    
    To exclude issues with writing to GITHUB_ENV, and see the java -version output
    
    * #156 other java var for j17
    
    need different # of lines of `java -version` output in different tests
    
    * fix matrix config ref
    
    * Use original env var from issue
    
    In a non-doc use case, so no fatal warnings generated
    
    * Avoid parsing java env vars with java version
    
    * Remove email as discussed
    
    * clean up
    
    * development version
    
    * Pr 156 feedback (#158)
    
    * Update ci-tests.yml
    
    * #156 explain the java version check

--- R/onLoad.R
+++ R/onLoad.R
@@ -29,11 +29,12 @@
 .onLoad <- function(libname, pkgname) {
   
   javaCheck <- function() {
-    # Java version check, without .jinit (we do .jpackage after downloading resources)
+    # Java version check, without initializing rJava (we can only do .jpackage(...) after downloading resources below)
     rawVersion <- system2("java", c("-version"), stdout = TRUE, stderr = TRUE)
-    jv <- regmatches(rawVersion[1], regexpr("[0-9\\.]+", rawVersion[1]))
+    versionLines <- rawVersion[!grepl("Picked up", rawVersion)] # ignore java environment variables being logged
+    jv <- regmatches(versionLines[1], regexpr("[0-9\\.]+", versionLines[1]))
     if (nchar(jv)<1) {
-      stop(paste0("unable to parse java version from", paste0(rawVersion, collapse=" "), "; is java installed correctly ?"))
+      stop(paste0("unable to parse java version from", paste0(versionLines, collapse=" "), "; is java installed correctly ?"))
     }
     else if(nchar(jv)==1) {
        jvn <- as.numeric(jv)
