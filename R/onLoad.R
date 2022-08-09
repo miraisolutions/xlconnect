ae7a0c1d42026bebe0f32acabfe697021b004cf3
spoltier <simon.poltier@mirai-solutions.com>
2022-08-09 12:28:56 +0200

    #168 fix copy paste, add poi full 5.2.2

--- R/onLoad.R
+++ R/onLoad.R
@@ -58,7 +58,7 @@
   sharedPaths <- tryCatch({
     c(
       xlcEnsureDependenciesFor(
-      paste0(apachePrefix, "/poi/poi-ooxml-schemas/5.2.2/poi-ooxml-schemas-5.2.2.jar"), "poi-ooxml-schemas.jar", 
+      paste0(apachePrefix, "/poi/poi-ooxml-full/5.2.2/poi-ooxml-full-5.2.2.jar"), "poi-ooxml-full.jar", 
       "5.2.2",  libname, pkgname, debianpkg = "libapache-poi-java", rpmpkg="apache-poi"),
     xlcEnsureDependenciesFor(
       paste0(apachePrefix, "/poi/poi-ooxml/5.2.2/poi-ooxml-5.2.2.jar"), "poi-ooxml.jar", 
@@ -79,7 +79,7 @@
       paste0(apachePrefix, "/commons/commons-math3/3.6.1/commons-math3-3.6.1.jar"), "commons-math3.jar",
       "3\\.([6-9]|1[0-9]).*",  libname, pkgname, debianpkg="libcommons-math3-java"),
     xlcEnsureDependenciesFor(
-      paste0(apachePrefix, "/logging/log4j/log4j-api/2.17.2/log4j-api-2.17.2.jar"), "xmlbeans.jar",
+      paste0(apachePrefix, "/logging/log4j/log4j-api/2.17.2/log4j-api-2.17.2.jar"), "log4j-api.jar",
       "2.17.2",  libname, pkgname),
     xlcEnsureDependenciesFor(
       paste0(repo, "/commons-codec/commons-codec/1.15/commons-codec-1.15.jar"), "commons-codec-1.15.jar",
@@ -87,9 +87,6 @@
     xlcEnsureDependenciesFor(
       paste0(repo, "/commons-io/commons-io/2.11.0/commons-io-2.11.0.jar"), "commons-io-2.11.0.jar",
       "2\\.(1[1-9]|[2-9][0-9]).*",  libname, pkgname, debianpkg="libcommons-io-java", rpmpkg="apache-commons-io"),
-    xlcEnsureDependenciesFor(
-      paste0(apachePrefix, "/poi/ooxml-schemas/1.4/ooxml-schemas-1.4.jar"), "ooxml-schemas.jar",
-      "1\\.([4-9]|[1-9][0-9]).*",  libname, pkgname),
     xlcEnsureDependenciesFor(
       paste0(repo, "/com/zaxxer/SparseBitSet/1.2/SparseBitSet-1.2.jar"), "SparseBitSet.jar",
       "1\\.([2-9]|[1-9][0-9]).*",  libname, pkgname)
