c17b7b9a5bafd7cea4e7d640aa8e8f722efe5f80
Peter Schmid <peter.schmid@mirai-solutions.com>
2022-12-06 13:23:27 +0100

    Add test file and small test to reproduce issue (#185)

---
+++ inst/unitTests/runit.workbook.testBug181.R
@@ -0,0 +1,38 @@
+#############################################################################
+#
+# XLConnect
+# Copyright (C) 2010-2021 Mirai Solutions GmbH
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+#############################################################################
+#
+# Test reproducing error log from issue 181 / PR 183
+# 
+# Author: Peter Schmid, Mirai Solutions GmbH
+#
+#############################################################################
+
+test.workbook.testBug181 <- function() {
+  # loading this workbook reproduces this error, but only the first time it is loaded during a session
+  # it seems however that this does not prevent the workbook from being read and even when the "error"
+  # is logged it isn't considered an exception and behaves more like a regular log message
+  checkNoException({
+    wb <- loadWorkbook(rsrc("resources/testBug181.xlsx"))
+    obj <- readWorksheet(wb, "Sheet1")
+    checkTrue(is.data.frame(obj))
+  })
+}
