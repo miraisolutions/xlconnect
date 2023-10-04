a679fabdc3bdd55a60c48508d03d04f3a4547321
Simon Poltier <simon.poltier@mirai-solutions.com>
2023-10-04 22:37:18 +0200

    poi 5.2.4 and its known dependencies (#200)
    * poi 5.2.4 and its known dependencies
    
    * update metadata files and clean up README

--- R/onLoad.R
+++ R/onLoad.R
@@ -58,16 +58,16 @@
   sharedPaths <- tryCatch({
     c(
       xlcEnsureDependenciesFor(
-      paste0(apachePrefix, "/poi/poi-ooxml-full/5.2.3/poi-ooxml-full-5.2.3.jar"), "poi-ooxml-full.jar", 
-      "5.2.3",  libname, pkgname),
+      paste0(apachePrefix, "/poi/poi-ooxml-full/5.2.4/poi-ooxml-full-5.2.4.jar"), "poi-ooxml-full.jar", 
+      "5.2.4",  libname, pkgname),
     xlcEnsureDependenciesFor(
-      paste0(apachePrefix, "/poi/poi-ooxml/5.2.3/poi-ooxml-5.2.3.jar"), "poi-ooxml.jar", 
-      "5.2.3",  libname, pkgname, debianpkg = "libapache-poi-java", rpmpkg="apache-poi"),
+      paste0(apachePrefix, "/poi/poi-ooxml/5.2.4/poi-ooxml-5.2.4.jar"), "poi-ooxml.jar", 
+      "5.2.4",  libname, pkgname, debianpkg = "libapache-poi-java", rpmpkg="apache-poi"),
     xlcEnsureDependenciesFor(
-      paste0(apachePrefix, "/poi/poi/5.2.3/poi-5.2.3.jar"), "poi.jar", 
-      "5.2.3",  libname, pkgname, debianpkg = "libapache-poi-java", rpmpkg="apache-poi"),
+      paste0(apachePrefix, "/poi/poi/5.2.4/poi-5.2.4.jar"), "poi.jar", 
+      "5.2.4",  libname, pkgname, debianpkg = "libapache-poi-java", rpmpkg="apache-poi"),
     xlcEnsureDependenciesFor(
-      paste0(apachePrefix, "/commons/commons-compress/1.21/commons-compress-1.21.jar"), "commons-compress.jar",
+      paste0(apachePrefix, "/commons/commons-compress/1.24.0/commons-compress-1.24.0.jar"), "commons-compress.jar",
       "1\\.(1[8-9]|[2-9][0-9]).*",  libname, pkgname, debianpkg = "libcommons-compress-java", rpmpkg="apache-commons-compress"),
     xlcEnsureDependenciesFor(
       paste0(apachePrefix, "/xmlbeans/xmlbeans/5.1.1/xmlbeans-5.1.1.jar"), "xmlbeans.jar",
@@ -79,16 +79,16 @@
       paste0(apachePrefix, "/commons/commons-math3/3.6.1/commons-math3-3.6.1.jar"), "commons-math3.jar",
       "3\\.([6-9]|1[0-9]).*",  libname, pkgname, debianpkg="libcommons-math3-java"),
     xlcEnsureDependenciesFor(
-      paste0(apachePrefix, "/logging/log4j/log4j-api/2.18.0/log4j-api-2.18.0.jar"), "log4j-api.jar",
+      paste0(apachePrefix, "/logging/log4j/log4j-api/2.20.0/log4j-api-2.20.0.jar"), "log4j-api.jar",
       "2.17.2",  libname, pkgname),
     xlcEnsureDependenciesFor(
-      paste0(repo, "/commons-codec/commons-codec/1.15/commons-codec-1.15.jar"), "commons-codec-1.15.jar",
+      paste0(repo, "/commons-codec/commons-codec/1.16.0/commons-codec-1.16.0.jar"), "commons-codec-1.16.0.jar",
       "1\\.(1[1-9]|[2-9][0-9]).*",  libname, pkgname, debianpkg="libcommons-codec-java", rpmpkg="apache-commons-codec"),
     xlcEnsureDependenciesFor(
-      paste0(repo, "/commons-io/commons-io/2.11.0/commons-io-2.11.0.jar"), "commons-io-2.11.0.jar",
+      paste0(repo, "/commons-io/commons-io/2.13.0/commons-io-2.13.0.jar"), "commons-io-2.13.0.jar",
       "2\\.(1[1-9]|[2-9][0-9]).*",  libname, pkgname, debianpkg="libcommons-io-java", rpmpkg="apache-commons-io"),
     xlcEnsureDependenciesFor(
-      paste0(repo, "/com/zaxxer/SparseBitSet/1.2/SparseBitSet-1.2.jar"), "SparseBitSet.jar",
+      paste0(repo, "/com/zaxxer/SparseBitSet/1.3/SparseBitSet-1.3.jar"), "SparseBitSet.jar",
       "1\\.([2-9]|[1-9][0-9]).*",  libname, pkgname)
     )
   },
