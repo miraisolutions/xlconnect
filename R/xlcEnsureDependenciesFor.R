f6c285594850953676ec0462c0f5b23871a7ce67
Martin Studer <martin.remo.studer@gmail.com>
2024-01-15 15:46:25 +0100

    Update xlconnect-java JAR; update copyright headers

--- R/xlcEnsureDependenciesFor.R
+++ R/xlcEnsureDependenciesFor.R
@@ -1,7 +1,7 @@
 #############################################################################
 #
 # XLConnect
-# Copyright (C) 2010-2020 Mirai Solutions GmbH
+# Copyright (C) 2010-2024 Mirai Solutions GmbH
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
