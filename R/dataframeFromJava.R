00df658df645b85ee2eaebab5756aab610343967
Simon Poltier <simon.poltier@mirai-solutions.com>
2024-01-23 08:50:35 +0000

    avoid overwriting names when setting attributes

--- R/dataframeFromJava.R
+++ R/dataframeFromJava.R
@@ -75,9 +75,10 @@ dataframeFromJava <- function(df, check.names) {
   	# Apply attributes
   	toSet = (attributes(df))[!names(attributes(df)) %in% c("jobj", "jclass", "class", "package")]
   	# Apply names
-  	names(res) = columnNames
+  	attributes(res) = toSet
+	names(res) = columnNames
   	result = data.frame(res, check.names = check.names, stringsAsFactors = FALSE)
-  	attributes(result) <- toSet
+  	
   	result
 	})
 }
